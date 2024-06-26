c8049deaee0717b5d641e6a45feb5c9d6eea0646
Haroldy Arturo Perez Rodriguez <arcademan@MacBook-Air-de-Haroldy.local>
2024-06-26 20:13:24 +0200

    fix -- Nextjs api CORS error

---
+++ src/app/api/service/ServicesAsyncRequest/route.tsx
@@ -0,0 +1,67 @@
+import {ServicesAsyncRequestInterface} from '@/interfaces/IServicesAsyncRequest'
+import { NextRequest } from 'next/server'
+
+// export default async function 
+// POST( req:NextApiRequest, res:NextApiResponse ) {
+
+//     const body: ServicesAsyncRequestInterface = req.body 
+//     const params = body.params as string
+//     const path = body.path as string
+//     const method = body.method as string
+//     const token = body.token as string
+
+//     try {
+
+//         const url = `${process.env.NEXT_BACKEND_ENDPOINT_URL}${path}` as string
+//         const request = await fetch( url, {
+//             method: method,
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 authorization: `Bearer ${token}`
+//             },
+//             body: params as string
+//         })
+    
+//         const response = await request.json()  
+
+//         return res.send(response)
+
+//     }
+
+//     catch ( error ) {
+//         return res.send(error)
+//     } 
+
+// }
+
+export async function POST( req:NextRequest ) {
+
+    const body = req.body 
+    const body_backend = JSON.parse(body as unknown as string) 
+    const path = body_backend.path as string
+    const method = body_backend.method as string
+    const token = body_backend?.token as string
+
+    try {
+
+        const url = `${process.env.NEXT_BACKEND_ENDPOINT_URL}${path}` as string
+        const request = await fetch( url, {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+                authorization: `Bearer ${token? token : ''}`
+            },
+            body: body_backend as string
+        })
+    
+        const response = await request.json()  
+
+        return response
+
+    }
+
+    catch ( error ) {
+        return error
+    } 
+
+}
