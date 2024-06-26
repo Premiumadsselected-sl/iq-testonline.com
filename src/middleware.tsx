0cd6fb8a9ca2b3347712d80296817bfb9866b8cf
Haroldy Arturo Perez Rodriguez <arcademan@MacBook-Air-de-Haroldy.local>
2024-06-26 21:10:17 +0200

    fix -- Nextjs api CORS error

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -3,10 +3,12 @@ export { default } from "next-auth/middleware"
 
 export const config = {
     matcher: [
-        //'/payment/:path*',
-        //'/thanks/:path*',
-        //'/profile/:path*',
-        //'/contact/:path*',
-        //'/training/:path*'
+        '/api/service/:path*',
+        '/api/service/*',
+        '/payment/:path*',
+        '/thanks/:path*',
+        '/profile/:path*',
+        '/contact/:path*',
+        '/training/:path*'
     ]
 }
