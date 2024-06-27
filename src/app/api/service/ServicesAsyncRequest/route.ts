d14315700965fa5f7012edbb541ef944e19d8527
Haroldy Arturo Perez Rodriguez <arcademan@MacBook-Air-de-Haroldy.local>
2024-06-27 03:46:47 +0200

    fix -- Nextjs api CORS error

--- src/app/api/service/ServicesAsyncRequest/route.ts
+++ src/app/api/service/ServicesAsyncRequest/route.ts
@@ -1,39 +1,6 @@
 // import {ServicesAsyncRequestInterface} from '@/interfaces/IServicesAsyncRequest'
 import { NextRequest, NextResponse } from 'next/server'
 
-// export default async function 
-// POST( req:NextApiRequest, res:NextApiResponse ) {
-
-//     const body: ServicesAsyncRequestInterface = req.body 
-//     const params = body.params as string
-//     const path = body.path as string
-//     const method = body.method as string
-//     const token = body.token as string
-
-//     try {
-
-//         const url = `${process.env.NEXT_BACKEND_ENDPOINT_URL}${path}` as string
-//         const request = await fetch( url, {
-//             method: method,
-//             headers: {
-//                 'Content-Type': 'application/json',
-//                 authorization: `Bearer ${token}`
-//             },
-//             body: params as string
-//         })
-    
-//         const response = await request.json()  
-
-//         return res.send(response)
-
-//     }
-
-//     catch ( error ) {
-//         return res.send(error)
-//     } 
-
-// }
-
 export async function GET( req:NextRequest, res: NextResponse ) {
 
     //  return NextResponse.json({ message: 'GET' })
@@ -55,9 +22,9 @@ export async function GET( req:NextRequest, res: NextResponse ) {
             body: body_backend as string
         })
     
-        const response = await request 
+        const response = await request.json()
 
-        return NextResponse.json(response)
+        return response
 
     }
 
