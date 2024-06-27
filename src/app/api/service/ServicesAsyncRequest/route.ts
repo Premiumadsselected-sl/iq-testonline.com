eab01ba3c52fee42ce5db94e19006fca296f4347
Haroldy Arturo Perez Rodriguez <arcademan@MacBook-Air-de-Haroldy.local>
2024-06-27 16:15:37 +0200

    fix -- Nextjs api CORS error

--- src/app/api/service/ServicesAsyncRequest/route.ts
+++ src/app/api/service/ServicesAsyncRequest/route.ts
@@ -5,29 +5,41 @@ export async function POST( req:NextRequest, res: NextResponse ) {
     
     const { path, params, token } = await req.json()
 
-    try {
+    const url = `${process.env.NEXT_BACKEND_ENDPOINT_URL}${path}` as string
+    const request = await fetch( url, {
+        method: params.method,
+        headers: {
+            'Content-Type': 'application/json',
+            authorization: `Bearer ${token? token : ''}`
+        },
+        body: params as string
+    })
 
-        const url = `${process.env.NEXT_BACKEND_ENDPOINT_URL}${path}` as string
-        const request = await fetch( url, {
-            method: params.method,
-            headers: {
-                'Content-Type': 'application/json',
-                authorization: `Bearer ${token? token : ''}`
-            },
-            body: params as string
-        })
+    const response = await request.json()
+
+    return NextResponse.json(response)
+
+    // try {
+
+    //     const url = `${process.env.NEXT_BACKEND_ENDPOINT_URL}${path}` as string
+    //     const request = await fetch( url, {
+    //         method: params.method,
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             authorization: `Bearer ${token? token : ''}`
+    //         },
+    //         body: params as string
+    //     })
     
-        const response = await request.json()
+    //     const response = await request.json()
 
-        console.log('response', response)
+    //     return NextResponse.json(response)
 
-        return NextResponse.json(response)
+    // }
 
-    }
-
-    catch ( error ) {
-        console.log('error', error)
-        return NextResponse.json(error)
-    } 
+    // catch ( error ) {
+    //     console.log('error', error)
+    //     return NextResponse.json(error)
+    // } 
 
 }
