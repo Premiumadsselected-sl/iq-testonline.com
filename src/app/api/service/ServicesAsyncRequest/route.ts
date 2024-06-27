ebfbe5e28d92d7cea35b30d9b73b6b7475a30ea6
Haroldy Arturo Perez Rodriguez <arcademan@MacBook-Air-de-Haroldy.local>
2024-06-27 14:59:09 +0200

    fix -- Nextjs api CORS error

--- src/app/api/service/ServicesAsyncRequest/route.ts
+++ src/app/api/service/ServicesAsyncRequest/route.ts
@@ -2,34 +2,33 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET( req:NextRequest, res: NextResponse ) {
+    return NextResponse.json({ message: 'GET', request: req })
+    // const body = req.body 
+    // const body_backend = JSON.parse(body as unknown as string) 
+    // const path = body_backend.path as string
+    // const method = body_backend.method as string
+    // const token = body_backend?.token as string
 
-    //  return NextResponse.json({ message: 'GET' })
-    const body = req.body 
-    const body_backend = JSON.parse(body as unknown as string) 
-    const path = body_backend.path as string
-    const method = body_backend.method as string
-    const token = body_backend?.token as string
+    // try {
 
-    try {
-
-        const url = `${process.env.NEXT_BACKEND_ENDPOINT_URL}${path}` as string
-        const request = await fetch( url, {
-            method: method,
-            headers: {
-                'Content-Type': 'application/json',
-                authorization: `Bearer ${token? token : ''}`
-            },
-            body: body_backend as string
-        })
+    //     const url = `${process.env.NEXT_BACKEND_ENDPOINT_URL}${path}` as string
+    //     const request = await fetch( url, {
+    //         method: method,
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //             authorization: `Bearer ${token? token : ''}`
+    //         },
+    //         body: body_backend as string
+    //     })
     
-        const response = await request.json()
+    //     const response = await request.json()
 
-        return response
+    //     return response
 
-    }
+    // }
 
-    catch ( error ) {
-        return error
-    } 
+    // catch ( error ) {
+    //     return error
+    // } 
 
 }
