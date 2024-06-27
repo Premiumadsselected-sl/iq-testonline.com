b8978264917af935fe8aac2977f34b77afa6e703
Haroldy Arturo Perez Rodriguez <arcademan@MacBook-Air-de-Haroldy.local>
2024-06-27 15:52:17 +0200

    fix -- Nextjs api CORS error

--- src/pages/services/iq-testonline/components/RegisterForm/CustomizeRegisterForm.tsx
+++ src/pages/services/iq-testonline/components/RegisterForm/CustomizeRegisterForm.tsx
@@ -47,7 +47,6 @@ export default function CustomizeRegisterForm({ pageProps }: Props) {
 
             const req_register = 
             await fetch(process.env.NEXT_PUBLIC_SERVICE_ENDPOINT_URL as string, {
-                method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ 
                     method: 'POST',
