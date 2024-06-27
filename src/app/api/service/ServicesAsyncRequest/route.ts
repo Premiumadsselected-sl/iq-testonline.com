b9e3be1cf918b3ce443b63dcb49e1c2796cbbb61
Haroldy Arturo Perez Rodriguez <arcademan@MacBook-Air-de-Haroldy.local>
2024-06-27 16:22:57 +0200

    fix -- Nextjs api CORS error

--- src/app/api/service/ServicesAsyncRequest/route.ts
+++ src/app/api/service/ServicesAsyncRequest/route.ts
@@ -6,18 +6,19 @@ export async function POST( req:NextRequest, res: NextResponse ) {
     const { path, params, token } = await req.json()
 
     const url = `${process.env.NEXT_BACKEND_ENDPOINT_URL}${path}` as string
-    const request = await fetch( url, {
-        method: params.method,
-        headers: {
-            'Content-Type': 'application/json',
-            authorization: `Bearer ${token? token : ''}`
-        },
-        body: params as string
-    })
+    return NextResponse.json({ url, params, token })
+    // const request = await fetch( url, {
+    //     method: params.method,
+    //     headers: {
+    //         'Content-Type': 'application/json',
+    //         authorization: `Bearer ${token? token : ''}`
+    //     },
+    //     body: params as string
+    // })
 
-    const response = await request.json()
+    // const response = await request.json()
 
-    return NextResponse.json(response)
+    // return NextResponse.json(response)
 
     // try {
 
@@ -39,7 +40,7 @@ export async function POST( req:NextRequest, res: NextResponse ) {
 
     // catch ( error ) {
     //     console.log('error', error)
-    //     return NextResponse.json(error)
+    //     return NextResponse.json({ error })
     // } 
 
 }
