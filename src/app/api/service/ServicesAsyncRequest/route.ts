80d5b415944b41a12711869882376d294b3fb168
blogginformore <blogginformore@gmail.com>
2024-06-28 17:02:42 +0200

    fix - api/service/ServicesAsyncRequest

--- src/app/api/service/ServicesAsyncRequest/route.ts
+++ src/app/api/service/ServicesAsyncRequest/route.ts
@@ -2,9 +2,33 @@ import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST( req:NextRequest, res: NextResponse ) {
 
-    return NextResponse.json({
-        method: "request.method",
-        count: 5
-    })
+    try {
+
+        const { method, path, params } = await req.json()
+        const url = `${process.env.NEXT_BACKEND_ENDPOINT_URL}${path}` as string
+        const token = req.headers.get('Authorization')
+
+        // const request = await fetch( url, {
+        //     method: method,
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'Authorization': `${token}`
+        //     },
+        //     body: JSON.stringify(params)
+        // })
+
+        // if (!request.ok) {
+        //     const errorResponse = await request.json()
+        //     return NextResponse.json(errorResponse, { status: request.status })
+        // }
+
+        // const response = await request.json()
+        return NextResponse.json(method)
+
+    }
+
+    catch ( error ) {
+        return NextResponse.json(error)
+    } 
 
 }
