ccf8bf0c36b349dd8cd80581308b6189ce277e4c
blogginformore <blogginformore@gmail.com>
2024-06-28 18:12:38 +0200

    fix

--- src/app/api/service/ServicesAsyncRequest/route.ts
+++ src/app/api/service/ServicesAsyncRequest/route.ts
@@ -3,26 +3,40 @@ import { NextRequest, NextResponse } from 'next/server'
 export async function POST( req:NextRequest, res: NextResponse ) {
 
     try {
-        const { method, path, params } = await req.json();
-        const url = `${process.env.NEXT_BACKEND_ENDPOINT_URL}${path}` as string;
-        const token = req.headers.get('Authorization');
 
-        const request = await fetch(url, {
+        const { method, path, params } = await req.json()
+        const url = `${process.env.NEXT_BACKEND_ENDPOINT_URL}${path}` as string
+        const token = req.headers.get('Authorization')
+       
+        // LOGGING
+        console.log('Backend Request:', {
+            url: url,
             method: method,
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
+                'Authorization': `${token}`
             },
-            body: JSON.stringify(params),
+            body: JSON.stringify(params)
         });
 
+        // Peticion al backend
+        const request = await fetch( url, {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `${token}`
+            },
+            body: JSON.stringify(params)
+        })
+
         if (!request.ok) {
-            const errorResponse = await request.json();
-            return NextResponse.json(errorResponse, { status: request.status });
+            const errorResponse = await request.json()
+            return NextResponse.json(errorResponse, { status: request.status })
         }
 
-        const response = await request.json();
-        return NextResponse.json(response);
+        const response = await request.json()
+        return NextResponse.json(response)
+
     }
 
     catch ( error ) {
@@ -30,12 +44,3 @@ export async function POST( req:NextRequest, res: NextResponse ) {
     } 
 
 }
-
-// {
-//     "method": "POST",
-//     "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFsZXh0ZXN0MUBtYWlsLmNvbSIsInJvbGUiOiJzdWJzY3JpYmVyIiwiaWF0IjoxNzE5NTg2ODcwLCJleHAiOjE3MTk2NzMyNzB9.1dUzhssvtpB5j5YePUR8PqJ5EJ7JTVXteubtuAcUsWU",
-//     "params": {
-//         "email": "alextest1@mail.com"
-//     },
-//     "path": "users/get-user"
-// }
