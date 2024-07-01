8d272a89de01427ae72adceba299bef44d6c687e
Haroldy Arturo Perez Rodriguez <arcademan@MacBook-Air-de-Haroldy.local>
2024-07-01 15:27:57 +0200

    add -- validaciones del modulo de pago

--- src/pages/services/iq-testonline/components/Payment/CustomizePaymentForm.tsx
+++ src/pages/services/iq-testonline/components/Payment/CustomizePaymentForm.tsx
@@ -45,7 +45,7 @@ export default function CustomizePaymentForm({ router, pageProps }: AppProps) {
                     <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
                         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                             <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-[50vw]">
-                                <div className='grid grid-cols-1 px-4 py-4 sm:px-8 sm:py-4 gap-6'>
+                                <div className='grid grid-cols-1 px-4 py-4 sm:px-8 sm:py-4 gap-6 max-h-96 overflow-y-scroll'>
                                     <GeneralConditions {...pageProps} />
                                     <div className=" inline-flex items-center">
                                         <label
