a0230299ed4b1ee0d38c40c3678b0912b9b82c28
Haroldy Arturo Perez Rodriguez <arcademan@MacBook-Air-de-Haroldy.local>
2024-07-01 13:02:27 +0200

    add -- validaciones del modulo de pago

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,10 +1,8 @@
-
 export { default } from "next-auth/middleware"
 
 export const config = {
     matcher: [
         '/payment/:path*',
-        '/thanks/:path*',
         '/profile/:path*',
         '/contact/:path*',
         '/training/:path*'
